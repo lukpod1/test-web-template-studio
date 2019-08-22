46c89452b403f65126b52608c8630ab941983060
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2019-08-21 21:29:07 -0300

    first commit

---
+++ my_account/src/App.jsx
@@ -0,0 +1,30 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Main from "./components/Main";
+import Grid2 from "./components/Grid2";
+import List from "./components/List";
+import Master_Detail from "./components/Master_Detail";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Redirect exact path = "/" to = "/Main" />
+          <Route path = "/Main" component = { Main } />
+          <Route path = "/Grid2" component = { Grid2 } />
+          <Route path = "/List" component = { List } />
+          <Route path = "/Master_Detail" component = { Master_Detail } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
